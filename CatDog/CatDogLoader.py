33692b928fe1987262cd3d7a2457d9968bbcd611
Benjamin Fattori <46356259+fattorib@users.noreply.github.com>
2020-08-18 10:53:23 -0400

    Add files via upload

--- CatDog/CatDogLoader.py
+++ CatDog/CatDogLoader.py
@@ -1,6 +1,27 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Aug 18 10:31:17 2020
+import torch
+from torchvision import datasets, transforms
+import helper
 
-@author: Ben
-"""
+train_root = 'Cat_Dog_Data/train'
+test_root = 'Cat_Dog_Data/test'
+
+'''
+Define:
+    1) Transformation pipeline, final step will be transferring to tensor
+    2) Load data using ImageFolder
+    3) Load into pytorch with Dataloader
+
+'''
+
+transform = transforms.Compose([transforms.Resize(255),
+                                transforms.CenterCrop(224),
+                                transforms.ToTensor()])
+
+
+traindata = datasets.ImageFolder(train_root,transform = transform)
+
+trainloader = torch.utils.data.DataLoader(traindata,batch_size = 32,shuffle = True)
+
+
+images, labels = next(iter(trainloader))
+helper.imshow(images[0], normalize=False)
